535a7b940372243f0945362b4f8c8715b71b90fc
magic8baller <lturis@gmail.com>
2019-02-24 21:28:41 -0500

    dom manip ui prepwork

--- public/scripts.js
+++ public/scripts.js
@@ -3,7 +3,22 @@ const socket = io('http://localhost:9001')
 //listen for nslist
 socket.on('nsList', nsData => {
   console.log('list of namespaces arrved')
-  console.log(nsData)
+  let namespaceDiv = document.querySelector('.namespaces')
+  namespaceDiv.innerHTML = ''
+  nsData.forEach(ns => {
+    namespaceDiv.innerHTML += `<div class='namespace' ns=${
+      ns.endpoint
+    }><img src=${ns.img} alt=${ns.endpoint}></div>`
+  })
+
+  //add clickevent for e/ NS
+  const namespaceDivs = Array.from(document.querySelectorAll('.namespace'))
+  namespaceDivs.forEach(el => {
+    el.addEventListener('click', e => {
+      const nsEndpoint = el.getAttribute('ns')
+      console.log(`${nsEndpoint} i should joinnnn`)
+    })
+  })
 })
 
 socket.on('messageFromServer', dataFromServer => {
